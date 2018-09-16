6f2482983182cbc7816d2a3f61a557f6c6ccffb7
rysdev <rysdeveloper@gmail.com>
2018-09-16 13:31:18 -0700

    Added admin controller

---
+++ InsuranceQuotes/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using InsuranceGenerator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsuranceGenerator.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var UserList = new List<AdminViewModel>();
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var users = db.Users;
+                foreach (var user in users)
+                {
+                    var userInfo = new AdminViewModel();
+
+                    var cars = from c in db.Cars
+                               where c.UserId == user.Id
+                               select c;
+
+                    userInfo.FirstName = user.FirstName;
+                    userInfo.LastName = user.LastName;
+                    userInfo.UserEmail = user.Email;
+                    userInfo.NumCars = cars.Count();
+
+                    userInfo.TotalRate = user.CompleteQuote;
+
+                    UserList.Add(userInfo);
+                }
+            }
+
+            return View(UserList);
+        }
+    }
+}
